23b2e28f855fd30f5d3c04f6392ac8d99b00ab02
BuildTools <unconfigured@null.spigotmc.org>
2025-11-21 11:07:25 -0300

    Actualización de la pagina account, nueva carpeta llamada (account) en src/assets

--- src/pages/Account.tsx
+++ src/pages/Account.tsx
@@ -1,20 +1,28 @@
 import { Link } from "react-router-dom"
+import default_user_account from "@/assets/account/default_user_icon.png"
 
 export default function Account() {
     return (
         <section className="max-w-3xl mx-auto">
-            <h1 className="text-2xl font-bold mb-4">Mi cuenta</h1>
+            <h1 className="text-2xl font-bold mb-5">Mi cuenta</h1>
 
-            <div className="grid gap-3">
-                <div className="p-4 rounded-lg bg-stone-900/40 border border-stone-700">
-                    <h2 className="font-semibold">Perfil</h2>
-                    <p className="text-sm text-stone-300 mt-2">Nombre: <strong>Usuario</strong></p>
-                    <p className="text-sm text-stone-300">Email: <strong>usuario@ejemplo.com</strong></p>
-                    <div className="mt-3">
-                        <Link to="/account/settings" className="text-amber-400 hover:underline text-sm">Editar perfil</Link>
-                    </div>
+            <div className="flex items-center justify-between mb-4">
+                <div>
+                    <h1 className="text-2x1 font-bold">Bienvenido, Usuario</h1>
+                    <h1 className="text-2x1"><strong>Nombre:</strong> Nombre Completo</h1>
+                    <h1 className="text-2x1"><strong>Email:</strong> usuario@ejemplo.com</h1>
+                    <h1 className="text-2x1"><strong>Direccion:</strong> Los trapenses 10</h1>
                 </div>
 
+                <img
+                    src={default_user_account}
+                    alt="Imagen de Usuario"
+                    className="h-30 w-30 rounded-full object-cover border-2 border-amber-400 bg-stone-400" />
+            </div>
+
+
+
+            <div className="grid gap-3">
                 <div className="p-4 rounded-lg bg-stone-900/40 border border-stone-700">
                     <h2 className="font-semibold">Órdenes</h2>
                     <p className="text-sm text-stone-300 mt-2">Aquí verás tus órdenes recientes.</p>
